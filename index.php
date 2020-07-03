2bc64819cbe10179e29c9914937a164760a68315
DavidStewartLDN <davidstewart.work@gmail.com>
2020-07-03 11:44:02 +0100

    first commit with base page working

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+<title>A nice title goes here!</title>
+</head>
+
+<body>
+<h1>
+Hello, World!
+</h1>
+<p>
+<?php
+ echo "Hello, this is PHP speaking!";
+?>
+</p>
+</body>
+</html>
